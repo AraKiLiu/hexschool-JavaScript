2c2c7a16489184a4290e115606c592c09184463c
Larry_LIu <hamster518@gmail.com>
2020-06-27 19:52:47 +0800

    0608 day1, 0609 day2

---
+++ daily task/0608 Day1/js/all.js
@@ -0,0 +1,13 @@
+var myCar = '我的車子';
+var registrationTime = '註冊時間';
+var stateAwake = true;
+var beerAtHome = 18;
+var beerBrand = '啤酒品牌';
+
+function getInformation() {}
+
+function uploadPictures() {}
+
+function updateScreen() {}
+
+function drinkＷine() {}
