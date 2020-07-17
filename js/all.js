98263f2b8772491afe32b1138f470b211a74092a
Larry_LIu <hamster518@gmail.com>
2020-07-17 18:02:37 +0800

    修正彈出視窗中checkbox錯誤

--- js/all.js
+++ js/all.js
@@ -2,34 +2,7 @@ new Vue({
   el: '#app',
   data: {
     // 產品列表
-    products: [
-      // {
-      //   id: 1122334455,
-      //   title: 'MacBook Pro13',
-      //   category: 'APPLE',
-      //   content:
-      //     '動森太MacBook Pro 配備第 10 代 4 核心 Intel 處理器，Turbo Boost 最高可達 3.8GHz。',
-      //   description: 'MacBook Pro13 銀色 512GB / 4核心第10代i5 / 16GB / 2.0GHz',
-      //   imageUrl: 'https://d.ecimg.tw/items/DYAJCSA900AN358/000001_1589257659.jpg',
-      //   enabled: 0,
-      //   origin_price: 57900,
-      //   price: 50000,
-      //   unit: '台',
-      // },
-      // {
-      //   id: 66778899,
-      //   title: 'MacBook Pro16',
-      //   category: 'APPLE',
-      //   content:
-      //     '配備改寫遊戲規則的效能、更寬廣的 16 吋 Retina 顯示器，以及滿足一天的電池續航力，全新 MacBook Pro 以強大為優先，讓想做的事，可以隨時隨地想到就做。',
-      //   description: 'MacBook Pro 16 銀色 6核心第九代 i7 / 512GB / 2.6GHz / 16GB',
-      //   imageUrl: 'https://c.ecimg.tw/items/DYAJBG1900AQJO6/000001_1593412632.jpg',
-      //   enabled: 1,
-      //   origin_price: 71668,
-      //   price: 70000,
-      //   unit: '台',
-      // },
-    ],
+    products: [],
     // 給彈出視窗使用的產品資料
     tempProduct: {},
     modalTitle: '',
@@ -61,6 +34,19 @@ new Vue({
         this.products = response.data.data; // 取得產品列表
         console.log(this.products);
         this.pagination = response.data.meta.pagination; // 取得分頁資訊
+      });
+    },
+    // 抓取單項產品
+    getProduct(id) {
+      const api = `https://course-ec-api.hexschool.io/api/${this.user.uuid}/admin/ec/product/${id}`;
+      //預設帶入 token
+      axios.defaults.headers['Authorization'] = `Bearer ${this.user.token}`;
+      axios.get(api).then((res) => {
+        // 取得成功後回寫到 tempProduct
+        this.tempProduct = res.data.data;
+        // 確保資料已經回寫後在打開 Modal
+        $('#productModal').modal('show');
+
       });
     },
     // 更新產品資料
@@ -102,8 +88,9 @@ new Vue({
         case 'edit':
           this.modalTitle = '編輯產品';
           // 將item做一個淺層複製到tempProduct
-          this.tempProduct = Object.assign({}, item);
-          $('#productModal').modal('show');
+          this.getProduct(item.id);
+          // this.tempProduct = Object.assign({}, item);
+          // $('#productModal').modal('show');
           break;
         case 'delete':
           // 開啟delProductModal視窗
